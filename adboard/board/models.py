d9b36e580742026b0981fc5e8670da36323ae02c
dernov01@mail.ru <StalkerOnline1998>
2022-09-28 15:30:59 +0500

    Update

--- adboard/board/models.py
+++ adboard/board/models.py
@@ -1,15 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
 from tinymce.models import HTMLField
-
-
-class User(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True,)
-    email_verify = models.BooleanField(default=False)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
 
 class Advertisement(models.Model):
