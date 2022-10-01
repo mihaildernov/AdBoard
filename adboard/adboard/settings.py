5f20caf0e1983d876386163d1583ce80bac1afd4
dernov01@mail.ru <StalkerOnline1998>
2022-10-01 10:50:03 +0500

    Update

--- adboard/adboard/settings.py
+++ adboard/adboard/settings.py
@@ -125,6 +125,10 @@ SITE_ID = 2
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
